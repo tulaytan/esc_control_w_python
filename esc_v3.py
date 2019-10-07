2b022ec8c90f1b2b0675598d0108b38f1cd1b135
EngineerDayi <engineerdayi1@gmail.com>
2019-10-07 08:34:02 +0300

    Bazı değişiklikler uygulandı.

--- esc_v3.py
+++ esc_v3.py
@@ -13,38 +13,44 @@ pi.set_servo_pulsewidth(ESC, 0)
 max_value = 1610 
 min_value = 1000
 
-def calibrate():   #This is the auto calibration procedure of a normal ESC
+def calibrate():   #Normal bir ESC icin otomatik kalibrasyon fonksiyonudur.
     pi.set_servo_pulsewidth(ESC, 0)
-    print("Disconnect the battery and press Enter")
+    print("Bataryayı çıkarın ve Enter'a basin.")
     inp = raw_input()
     if inp == '':
         pi.set_servo_pulsewidth(ESC, max_value)
-        print("Connect the battery NOW.. you will here two beeps, then wait for a gradual falling tone then press Enter")
+        print("Bataryayı takin ve Enter'a basin.")
         inp = raw_input()
         if inp == '':            
             pi.set_servo_pulsewidth(ESC, min_value)
-            print "Wierd eh! Special tone"
+            print "Ozel tonda bir ses gelmeli."
             time.sleep(7)
-            print "Wait for it ...."
+            print "Biraz bekleyin..."
             time.sleep (5)
-            print "Im working on it, DONT WORRY JUST WAIT....."
+            print "Endiselenme :) Calisiyorum..."
             pi.set_servo_pulsewidth(ESC, 0)
             time.sleep(2)
-            print "Arming ESC now..."
+            print "ESC simdi baslatiliyor."
             pi.set_servo_pulsewidth(ESC, min_value)
             time.sleep(1)
-            print "See.... uhhhhh"
-            ser = serial.Serial('/dev/ttyUSB0', 9600,timeout = .15)
+            print "------"
+            '''
+            XBee pc ye explorer ile baglandigindan seri okuma yapilmali.
+            '''
+            ser = serial.Serial('/dev/ttyUSB0', 9600,timeout = .15)  
             while True:
                 response = ser.readline().strip()
                 data = response.decode("utf-8")
-                if (data != ""):
+                if (data != ""):  #Data kaybolmasina karsi onlem
                     speed_data = int(data)
-                    print(speed_data)
-                    print(type(speed_data))
-                    pi.set_servo_pulsewidth(ESC, speed_data)
+                    #print(speed_data)
+                    #print(type(speed_data))
+                    pi.set_servo_pulsewidth(ESC, speed_data) 
                         
             
 calibrate()
+'''
+While True dongusu son kisma alininca hatalar alindigi icin kalibre fonksiyonu icine yazildi.
+'''
 
     
